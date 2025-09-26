2a6d4231fb46cbd6a2b545fa452a8565448a7d65
gnaneshwar04 <gnaneshwarreddy0816o@gmail.com>
2025-09-26 17:26:28 +0530

    Configure application for Heroku depoyment

--- database.py
+++ database.py
@@ -1,19 +1,22 @@
+# database.py
+import os # <-- ADD THIS IMPORT
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Define the database URL. For SQLite, this is the path to the database file.
-# "./booking.db" means the file will be created in the same directory as your project.
-SQLALCHEMY_DATABASE_URL = "sqlite:///./booking.db"
+# Heroku provides the DATABASE_URL. If not found, we use our local SQLite DB.
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./booking.db")
 
-# Create the SQLAlchemy engine. The engine is the entry point to the database.
+# Heroku's postgres URL starts with 'postgres://', but SQLAlchemy needs 'postgresql://'
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+# Modify the engine creation
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    # For SQLite only, we need connect_args. For Postgres, we don't.
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 )
-# The connect_args is needed only for SQLite to allow multiple threads to interact with the database.
 
-# Create a SessionLocal class. Each instance of a SessionLocal class will be a database session.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Create a Base class. Our database models will inherit from this class.
 Base = declarative_base()
